a378e1ef4aa56325fce02f336629ab629d83da93
Shri Krishan <39910067+sk2-2@users.noreply.github.com>
2019-06-02 10:55:53 +0530

    Initial commit

---
+++ car detection.py
@@ -0,0 +1,27 @@
+import cv2
+
+cascade_src="cars.xml"
+video_src="video.avi"
+
+cap=cv2.VideoCapture(video_src)
+
+car_cascade=cv2.CascadeClassifier(cascade_src)
+
+while True:
+    ret,img=cap.read()
+
+    if(type(img)==type(None)):
+        break
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    cars= car_cascade.detectMultiScale(gray,1.1,1)
+
+    for(x,y,w,h) in cars:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+
+    cv2.imshow("video",img)
+
+    k=cv2.waitKey(5)
+    if(k==ord("q")):
+        break
+cv2.destroyAllWindows()
+
